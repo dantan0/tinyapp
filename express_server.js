1408cd2c38f41cddbdb21c9817eea80b9d1e8a13
dantan123 <dtan9988@gmail.com>
2021-01-18 23:01:04 +0000

    add html body response

--- express_server.js
+++ express_server.js
@@ -13,10 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/urls', (req, res) => {
   res.send(urlDatabase);
-})
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Examine app listening on port ${PORT}!`);
 });
 
-console.log('first to log!')
+console.log('first to log!');
