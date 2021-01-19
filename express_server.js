93a9069ec63cfb160e6cfd282cd660843b4642f8
dantan123 <dtan9988@gmail.com>
2021-01-19 03:18:35 +0000

    add post request and response and rand string generation function

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
-// use ejs
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+
+// purpose: return a string of 6 random alphanumeric characters
+// source: https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2);
 };
 
 app.get('/', (req, res) => {
@@ -19,12 +26,23 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+// new needs to be defined before the specific url id
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   const templateVars = { shortURL, longURL };
   res.render('urls_show', templateVars);
 });
+
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
