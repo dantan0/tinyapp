886d0ecc8417e87b572bcacdbed1a96f4bb6a419
dantan123 <dtan9988@gmail.com>
2021-01-19 02:09:45 +0000

    add template rendering

--- express_server.js
+++ express_server.js
@@ -3,20 +3,27 @@ const app = express();
 const PORT = 8080;
 
 // use ejs
-app.set('view-engine', 'ejs');
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
 app.get('/urls', (req, res) => {
-  res.send(urlDatabase);
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  const templateVars = { shortURL, longURL };
+  res.render('urls_show', templateVars);
 });
 
 app.get('/hello', (req, res) => {
