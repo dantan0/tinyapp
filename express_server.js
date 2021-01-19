23880cbef9467d8c1ce1064e7a26b55adcbd6a5f
dantan123 <dtan9988@gmail.com>
2021-01-19 00:06:44 +0000

    install ejs and use

--- express_server.js
+++ express_server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// use ejs
+app.set('view-engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get('/', (req, res) => {
   res.send('Hello!');
